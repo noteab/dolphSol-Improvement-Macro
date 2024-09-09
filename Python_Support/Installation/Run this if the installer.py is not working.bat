cfea9eb3b320b44b0e5af83ae0e33034fa479002
steveonly1 <163425038+suiyang12@users.noreply.github.com>
2024-09-09 17:08:05 +0530

    Delete Python_Support/Installation/Run this if the installer.py is not working.bat

--- Python_Support/Installation/Run this if the installer.py is not working.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing setuptools via pip...
-python -m pip install --upgrade pip
-python -m pip install setuptools
-echo Installation complete!
-echo.
-echo Now try running installer.py and finish the installation.
-pause
